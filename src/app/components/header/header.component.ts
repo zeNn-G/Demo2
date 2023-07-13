8fcabc9a653549fb950443c23c01a8e0e00cb75f
zeNn-G <treroin@gmail.com>
2023-07-13 09:57:51 +0300

    Remove Focus on Submit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -8,16 +8,22 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @ViewChild('inputRef', { static: false })
+  inputRef!: ElementRef<HTMLInputElement>;
+
   searchForm = new FormGroup({
     searchQuery: new FormControl(''),
   });
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private elementRef: ElementRef) {}
 
   onSubmit() {
     this.router.navigate(['/search'], {
       queryParams: { q: this.searchForm.value.searchQuery },
     });
+
+    //Removes Focus
+    this.inputRef.nativeElement.blur();
 
     this.searchForm.reset();
   }
